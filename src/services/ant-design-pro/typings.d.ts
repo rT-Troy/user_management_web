7e4b1e20ab6c0a1b7cd18c2d80f1c589f5de4a11
rT-Troy <yotroy@foxmail.com>
2024-09-09 23:32:03 +0800

    Make avatar and watermark works; rebuild CurrentUser type api.

--- src/services/ant-design-pro/typings.d.ts
+++ src/services/ant-design-pro/typings.d.ts
@@ -3,24 +3,38 @@
 
 declare namespace API {
   type CurrentUser = {
-    name?: string;
-    avatar?: string;
-    userid?: string;
-    email?: string;
-    signature?: string;
-    title?: string;
-    group?: string;
-    tags?: { key?: string; label?: string }[];
-    notifyCount?: number;
-    unreadCount?: number;
-    country?: string;
-    access?: string;
-    geographic?: {
-      province?: { label?: string; key?: string };
-      city?: { label?: string; key?: string };
-    };
-    address?: string;
+    id?: string;  // all of these should be string expect date
+    username?: string;
+    userAccount?: string;
+    userPassword?: string;
+    avatarUrl?: string;
+    gender?: string;
     phone?: string;
+    email?: string;
+    userStatus?: string;
+    createTime?: Date;
+    update?: string;
+    isDelete?: string;
+    userRole?: string;
+
+    // name?: string;
+    // avatar?: string;
+    // userid?: string;
+    // email?: string;
+    // signature?: string;
+    // title?: string;
+    // group?: string;
+    // tags?: { key?: string; label?: string }[];
+    // notifyCount?: number;
+    // unreadCount?: number;
+    // country?: string;
+    // access?: string;
+    // geographic?: {
+    //   province?: { label?: string; key?: string };
+    //   city?: { label?: string; key?: string };
+    // };
+    // address?: string;
+    // phone?: string;
   };
 
   type LoginResult = {
